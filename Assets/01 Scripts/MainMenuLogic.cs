70de8ec3e35f05d23911cb1eb2af6d0b887a52b3
RyanRules23 <73117357+RyanRules23@users.noreply.github.com>
2021-09-28 14:55:54 -0500

    Mainmenu hook up
    allows the player to enter into the programming playground when pressing the new game button

---
+++ Assets/01 Scripts/MainMenuLogic.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuLogic : MonoBehaviour
+{
+    //temporary code for testing the programming playground
+ public void _PLayDemo()
+    {
+        SceneManager.LoadScene("Programming PLayground");
+    }
+}
