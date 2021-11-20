909d05df101cb374484b607415c43c83106b066c
LemonGraabTheForsaken <tylerhaley914@gmail.com>
2021-11-19 22:03:51 -0600

    I am once again asking for your support
    I once again did some audio Manager stuff that shouldn't affect anybody until its finished. Which I think its really close to being finished.

---
+++ Assets/01 Scripts/Audio/LoadScene.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LoadScene : MonoBehaviour
+{
+    public void GoToScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+}
