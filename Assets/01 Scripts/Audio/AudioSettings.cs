cf04027401a383cc415ac6a7a4ca4c05fcf52919
LemonGraabTheForsaken <tylerhaley914@gmail.com>
2021-11-23 14:32:35 -0600

    audio manager stuff
    got the auduio mangager done

--- Assets/01 Scripts/Audio/AudioSettings.cs
+++ Assets/01 Scripts/Audio/AudioSettings.cs
@@ -2,39 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AudioSettings : MonoBehaviour
+namespace Harpaesis.AudioSystem
 {
-    private static readonly string BackgroundPref = "BackgroundPref";
-    private static readonly string SoundEffectsPref = "SoundEffectsPref";
-    private static readonly string MasterVolumePref = "MasterVolumePref";
-    private float backgroundFloat, soundEffectsFloat;
-    [SerializeField] private float masterVolFloat;
-    public AudioSource[] backgroundMusicAudio;
-    public AudioSource[] soundEffectsAudio;
 
-    private void Awake()
+    public class AudioSettings : MonoBehaviour
     {
-        ContinueSettings();
-    }
+        private static readonly string BackgroundPref = "BackgroundPref";
+        private static readonly string SoundEffectsPref = "SoundEffectsPref";
+        private static readonly string MasterVolumePref = "MasterVolumePref";
+        private float backgroundFloat, soundEffectsFloat;
+        private float masterVolFloat;
+        public AudioSource[] backgroundMusicAudio;
+        public AudioSource[] soundEffectsAudio;
 
-    private void ContinueSettings()
-    {
-        backgroundFloat = PlayerPrefs.GetFloat(BackgroundPref);
-        soundEffectsFloat = PlayerPrefs.GetFloat(SoundEffectsPref);
-        masterVolFloat = PlayerPrefs.GetFloat(MasterVolumePref);
-
-
-        for (int i = 0; i < backgroundMusicAudio.Length; i++)
+        private void Awake()
         {
-            backgroundMusicAudio[i].volume = backgroundFloat;
+            ContinueSettings();
         }
 
-        for (int i = 0; i < soundEffectsAudio.Length; i++)
+        private void ContinueSettings()
         {
-            soundEffectsAudio[i].volume = soundEffectsFloat;
+            backgroundFloat = PlayerPrefs.GetFloat(BackgroundPref);
+            soundEffectsFloat = PlayerPrefs.GetFloat(SoundEffectsPref);
+            masterVolFloat = PlayerPrefs.GetFloat(MasterVolumePref);
+
+
+            for (int i = 0; i < backgroundMusicAudio.Length; i++)
+            {
+                backgroundMusicAudio[i].volume = backgroundFloat;
+            }
+
+            for (int i = 0; i < soundEffectsAudio.Length; i++)
+            {
+                soundEffectsAudio[i].volume = soundEffectsFloat;
+            }
+
+            AudioListener.volume = masterVolFloat;
+
         }
-
-        //AudioListener.volume = masterVolFloat;
-
     }
 }
