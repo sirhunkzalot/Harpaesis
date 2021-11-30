14a63e62f737fd7c2e5a0fb362bdc0ce88721f2e
Ubaralook <50388871+Ubaralook@users.noreply.github.com>
2021-11-30 16:43:11 -0600

    Added HP Bars to all enemies

---
+++ Assets/Universal Expandable Progress Bar/Scripts/ColoredProgressBar.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public class ColoredProgressBar : ProgressBar {
+
+    [SerializeField] private Color finalColor;
+    [SerializeField] private Color startColor;
+
+    private bool maxGained;
+
+    public override ProgressBar SetColor(Color c) {
+        finalColor = c;
+        return this;
+    }
+
+    public ProgressBar SetFinalColor(Color c) {
+        finalColor = c;
+        return this;
+    }
+
+    public ProgressBar SetStartColor(Color c) {
+        startColor = c;
+        return this;
+    }
+
+    protected override void CalculateProgress() {
+        if (IsMinMax()) return;
+        if (smoothly) {
+            float fillNeed = currentProgress / (float)max;
+            float currentFill = Mathf.Lerp(progress.fillAmount, fillNeed, smoothTime*Time.deltaTime);
+            progress.fillAmount = currentFill;
+            facticalProgress = Mathf.RoundToInt(currentFill * max);
+        } else {
+            facticalProgress = currentProgress;
+            progress.fillAmount = facticalProgress / (float)max;
+        }
+
+        float progressForColor = facticalProgress / (float) max;
+        progress.color = startColor * (1 - progressForColor) + finalColor * progressForColor;
+
+        text.text = beforeText + facticalProgress + afterText;
+
+        if (!maxGained) {
+            if (GetProgress() == (float)max) {
+                maxGained = true;            
+                text.color = Color.Lerp(startColor, finalColor, Mathf.PingPong(Time.time*2,1));
+            } else {
+                text.color = startColor;
+            }
+        } else {
+            if (GetProgress() < (float)max) {
+                maxGained = false;             
+                text.color = startColor;
+            } else {
+                text.color = Color.Lerp(startColor, finalColor,Mathf.PingPong(Time.time*2,1));
+            }
+         
+        }
+    }
+
+}
---
+++ Assets/Universal Expandable Progress Bar/Scripts/ProgressBar.cs
@@ -0,0 +1,157 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+[ExecuteInEditMode]
+[System.Serializable]
+public class ProgressBar : MonoBehaviour {
+    private List<OnCompleteListener> listeners = new List<OnCompleteListener>();
+
+    [Tooltip("Image for progress bar.")]
+    [SerializeField]
+    protected Image progress;
+    [Tooltip("Text with current progressbar caption.")]
+    [SerializeField]
+    protected Text text;
+
+    [Tooltip("Text that will appear before progress bar value.")]
+    [SerializeField]
+    protected string beforeText = "";
+
+    [Tooltip("Text that will appear after progress bar value.")]
+    [SerializeField]
+    protected string afterText = "%";
+
+    [Tooltip("Max possible progress.")]
+    [SerializeField]
+    protected int max = 100;
+
+    [Range(1, 10)]
+	[SerializeField]
+    protected float smoothTime = 2;
+
+    protected bool smoothly;
+    protected int facticalProgress;
+    [SerializeField]
+    protected int currentProgress;
+
+    [SerializeField]
+    private bool isFull;
+
+    void Start() {
+        progress.fillAmount = 0;
+    }
+
+    void Update() {
+        CalculateProgress();
+        NotifyListeners();
+    }
+
+    public int GetProgress() {
+        return facticalProgress;
+    }
+
+    public void SetProgress(int progress) {
+        smoothly = false;
+        currentProgress = progress;
+        CalculateProgress();
+    }
+
+    public void SetProgress(int progress, bool smooth) {
+        smoothly = smooth;
+        currentProgress = progress;
+    }
+
+    public virtual ProgressBar SetColor(Color c) {
+        progress.color = c;
+        return this;
+    }
+
+    public ProgressBar SetTextSize(int size) {
+        text.fontSize = size;
+        return this;
+    }
+
+    public virtual ProgressBar SetAfterText(string text) {
+        afterText = text;
+        return this;
+    }
+
+    public ProgressBar SetMax(int max) {
+        if (max < 0) {
+            return this;
+        }
+        this.max = max;
+        if (facticalProgress > max) {
+            facticalProgress = max;
+        }
+        if (currentProgress > max) {
+            currentProgress = max;
+        }
+        return this;
+    }
+
+    public virtual ProgressBar SetBeforeText(string text) {
+        beforeText = text;
+        return this;
+    }
+
+    public string GetBeforeText() {
+        return beforeText;
+    }
+
+    public ProgressBar addListener(OnCompleteListener listener) {
+        if (!listeners.Contains(listener)) {
+            listeners.Add(listener);
+        }
+        return this;
+    }
+
+    public ProgressBar removeListener(OnCompleteListener listener) {
+        if (!listeners.Contains(listener)) {
+            listeners.Remove(listener);
+        }
+        return this;
+    }
+
+    protected virtual void CalculateProgress() {
+        if (IsMinMax()) return;
+        if (smoothly) {
+            float fillNeed = currentProgress / (float)max;
+            float currentFill = Mathf.Lerp(progress.fillAmount, fillNeed, smoothTime * Time.unscaledDeltaTime);
+            progress.fillAmount = currentFill;
+            facticalProgress = Mathf.RoundToInt(currentFill * max);
+        } else {
+            facticalProgress = currentProgress;
+            progress.fillAmount = facticalProgress / (float)max;
+        }
+        if (text != null) {
+            text.text = beforeText + facticalProgress + afterText;
+        }
+    }
+
+    protected bool IsMinMax() {
+        if (currentProgress > max) {
+            currentProgress = max;
+            return true;
+        }
+        if (currentProgress < 0) {
+            currentProgress = 0;
+            return true;
+        }
+        return false;
+    }
+
+    private void NotifyListeners() {
+        if (facticalProgress == max && !isFull) {
+            isFull = true;
+            foreach (OnCompleteListener l in listeners) {
+                if (l != null) {
+                    l.progressBarComplete();
+                }
+            }
+        } else if (facticalProgress < max) {
+            isFull = false;
+        }
+    }
+}
