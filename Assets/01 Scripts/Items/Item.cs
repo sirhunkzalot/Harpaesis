e40e2b779f9392b9816366ce684c3970bedb5575
Matthew Sommer <msommer@stumail.jccc.edu>
2022-01-29 15:09:36 -0600

    Item Tweaking

--- Assets/01 Scripts/Items/Item.cs
+++ Assets/01 Scripts/Items/Item.cs
@@ -8,12 +8,17 @@ namespace Harpaesis.Inventory
     public abstract class Item : ScriptableObject
     {
         public string itemName;
+        public int itemCost = 5;
+        [TextArea(3,5)] public string itemDescription;
+
+        [Space]
         public Sprite itemSprite;
-        public List<SkillEffect> skillEffects = new List<SkillEffect>();
+        public GameObject itemShopPrefab;
+        public List<SkillEffect> itemEffect = new List<SkillEffect>();
 
         public virtual void UseItem(Unit _unit)
         {
-            SkillEffectLibrary.ResolveEffects(_unit, _unit, skillEffects);
+            SkillEffectLibrary.ResolveEffects(_unit, _unit, itemEffect);
         }
     }
 
