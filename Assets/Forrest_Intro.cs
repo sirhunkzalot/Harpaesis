b12e642955d3c79ea30f01a3afe034faaaed3148
RyanRules23 <73117357+RyanRules23@users.noreply.github.com>
2022-03-02 10:57:20 -0600

    temporary skip lines

--- Assets/Forrest_Intro.cs
+++ Assets/Forrest_Intro.cs
@@ -6,6 +6,10 @@ using UnityEngine.SceneManagement;
 public class Forrest_Intro : MonoBehaviour
 {
     public float waitTime = 70f;
+
+    public GameObject paragraphOne;
+    public GameObject paragraphTwo;
+    public GameObject paragraphThree;
     // Update is called once per frame
     void Update()
     {
@@ -15,5 +19,23 @@ public class Forrest_Intro : MonoBehaviour
         {
             SceneManager.LoadScene(2);
         }
+    }
+
+    public void SkipLines()
+    {
+        if(paragraphOne.activeSelf)
+        {
+            paragraphOne.SetActive(false);
+        }
+        if (paragraphTwo.activeSelf)
+        {
+            paragraphTwo.SetActive(false);
+        }
+        if (paragraphThree.activeSelf)
+        {
+            paragraphThree.SetActive(false);
+        }
+
+        SceneManager.LoadScene(2);
     }
 }
